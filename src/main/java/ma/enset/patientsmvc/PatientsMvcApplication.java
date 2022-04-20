b8da66c1e213483266aa385d9a38e0e5cba6a39d
RABI_YOUSSEF <RABI_YOUSSEF@emsi-edu.ma>
2022-04-20 23:37:00 +0200

    Initial commit

---
+++ src/main/java/ma/enset/patientsmvc/PatientsMvcApplication.java
@@ -0,0 +1,38 @@
+package ma.enset.patientsmvc;
+
+import ma.enset.patientsmvc.entities.Patient;
+import ma.enset.patientsmvc.repositories.PatientRepositories;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Date;
+
+@SpringBootApplication
+public class PatientsMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PatientsMvcApplication.class, args);
+
+    }
+    @Bean
+    CommandLineRunner commandLineRunner(PatientRepositories patientRepositories){
+        return args ->{
+          patientRepositories.save(
+                  new Patient(null,"youssef",new Date(),false,12));
+            patientRepositories.save(
+                    new Patient(null,"mohammed",new Date(),true,321));
+            patientRepositories.save(
+                    new Patient(null,"yassine",new Date(),true,616));
+            patientRepositories.save(
+                    new Patient(null,"imane",new Date(),false,9430));
+            patientRepositories.findAll().forEach(p->{
+                System.out.println(p.getNom());
+            });
+
+        };
+    }
+
+}
+
